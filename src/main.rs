639c5de13f55cc286e89fe25d5a50c677117aefa
Antoni <Antoni Szczepanik>
2020-10-14 17:20:10 +0200

    Add first version of init command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::env;
+use std::fs;
+use std::io::ErrorKind;
+
+static HIDDEN_DIR: &str = "git-rust-root";
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+    match command.as_str() {
+        // no arguments passed
+        "init" => init(),
+        _ => println!("'{}' is not implemented yet :(", command),
+    }
+}
+
+fn init() {
+    println!("We're initing! :)");
+    let dir = fs::create_dir(HIDDEN_DIR);
+    match dir {
+        Ok(dir) => dir,
+        Err(error) => match error.kind() {
+            ErrorKind::AlreadyExists => {
+                println!("{} has already been initialized.", HIDDEN_DIR);
+            },
+            other_error => {
+                panic!("Problem creating {}: {:?}", HIDDEN_DIR,  other_error);
+            }
+        },
+    };
 }
