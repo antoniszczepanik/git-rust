8e591ad466b14e8e0485cb6bbbc4d897ce999016
Antoni <Antoni Szczepanik>
2020-11-16 01:03:32 +0100

    Create basic directories with init, placeholders for hash-object and helps

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,66 @@
 use std::env;
 use std::fs;
-use std::io::ErrorKind;
+use std::path::Path;
+use std::string::String;
 
-static HIDDEN_DIR: &str = "git-rust-root";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let command = &args[1];
-    match command.as_str() {
-        // no arguments passed
+    let a: Args = Args::new(&args);
+    match a.command.as_str() {
         "init" => init(),
-        _ => println!("'{}' is not implemented yet :(", command),
+        "hash-object" => hash_object(a.filename.as_str()),
+        "help" => display_help(),
+        _ => println!("Not implemented"),
     }
 }
 
-fn init() {
-    println!("We're initing! :)");
-    let dir = fs::create_dir(HIDDEN_DIR);
-    match dir {
-        Ok(dir) => dir,
-        Err(error) => match error.kind() {
-            ErrorKind::AlreadyExists => {
-                println!("{} has already been initialized.", HIDDEN_DIR);
-            },
-            other_error => {
-                panic!("Problem creating {}: {:?}", HIDDEN_DIR,  other_error);
+struct Args {
+    command: String,
+    filename: String,
+}
+
+impl Args {
+    fn new(args: &[String]) -> Args {
+        if args.len() == 2 {
+            Args { 
+                command: args[1].clone(),
+                filename: "".to_string(),
             }
-        },
+        } else if args.len() == 3 {
+            Args { 
+                command: args[1].clone(),
+                filename: args[2].clone(),
+            }
+        } else {
+            panic!("This number of arguments is not supported");
+        }
+    }
+}
+
+
+fn init() {
+    create_dir(".rgit");
+    create_dir(".rgit/objects");
+}
+
+fn hash_object(filename: &str) {
+   if Path::new(filename).exists() {
+        read_file_contents();
+   }
+
+}
+
+fn read_file_contents() {
+}
+
+fn create_dir(dir_to_create: &str) {
+    match fs::create_dir(dir_to_create) {
+        Ok(_v) => println!("created dir {}", dir_to_create),
+        Err(_e) => println!("error creating {}", dir_to_create),
     };
+}
+
+fn display_help() {
+    println!("TODO: Help message should be displayed");
 }
